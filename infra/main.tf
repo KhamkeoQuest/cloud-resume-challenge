2507700adb7458d4bd85f090cab374be949f98f1
KhamkeoQuest <khamkeo@gmail.com>
2025-06-04 12:26:58 -0700

    intial build created.  testing now

--- infra/main.tf
+++ infra/main.tf
@@ -2,41 +2,27 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-resume-prod-wus3"
-  location = "West US 3"
-  tags = {
-  environment = "prod"
-  project     = "cloud-resume"
-}
+  for_each = {
+    dev   = var.locations["dev"]
+    stage = var.locations["stage"]
+    prod1 = var.locations["prod1"]
+    prod2 = var.locations["prod2"]
+  }
 
-}
+  name     = "rg-${var.project_name}-${each.key}"
+  location = each.value
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-resume-prod-scus"
-  location = "South Central US"
-  tags = {
-  environment = "prod"
-  project     = "cloud-resume"
+  tags = lookup(var.tags, (
+    contains(["prod1", "prod2"], each.key) ? "prod" : each.key
+  ), {})
 }
-
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "crstorage${random_id.unique.hex}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
-}
-
-resource "azurerm_storage_table" "counter" {
-  name                 = "VisitorCounter"
-  storage_account_name = azurerm_storage_account.storage.name
-  resource_group_name  = azurerm_resource_group.rg.name
-}
-resource "azurerm_storage_table" "resume" {
-  name                 = "Resume"
-  storage_account_name = azurerm_storage_account.storage.name
-  resource_group_name  = azurerm_resource_group.rg.name
-} 
---
+++ infra/variables.tf
@@ -0,0 +1,38 @@
+variable "environments" {
+  type    = list(string)
+  default = ["dev", "stage", "prod"]
+}
+
+variable "locations" {
+  type    = map(string)
+  default = {
+    dev   = "East US"
+    stage = "Central US"
+    prod1 = "West US 3"
+    prod2 = "South Central US"
+  }
+}
+
+variable "project_name" {
+  type    = string
+  default = "resume"
+}
+
+variable "tags" {
+  description = "Tag map per environment"
+  type = map(map(string))
+  default = {
+    dev = {
+      environment = "dev"
+      project     = "cloud-resume"
+    }
+    stage = {
+      environment = "stage"
+      project     = "cloud-resume"
+    }
+    prod = {
+      environment = "prod"
+      project     = "cloud-resume"
+    }
+  }
+}
