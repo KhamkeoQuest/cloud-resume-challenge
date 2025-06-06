b84d67e5ba9b6c775c375d99eb24fc02e2d02b1b
KhamkeoQuest <khamkeo@gmail.com>
2025-06-06 10:52:28 -0700

    steup stage and prod like dev

---
+++ infra/environments/prod/outputs.tf
@@ -0,0 +1,24 @@
+output "static_site_url" {
+  description = "The primary endpoint of the static website"
+  value       = module.static_site.static_site_url
+}
+
+output "storage_account_name" {
+  value = module.static_site.storage_account_name
+}
+
+output "web_container_name" {
+  value = "$web"
+}
+
+output "debug_index_path" {
+  value = local.index_path
+}
+
+output "debug_resume_exists" {
+  value = fileexists(local.resume_path)
+}
+
+output "resolved_resume_path" {
+  value = abspath(local.resume_path)
+}
--- infra/environments/stage/main.tf
+++ infra/environments/stage/main.tf
@@ -1,32 +1,30 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  required_version = ">= 1.0.0"
-}
+# stage/main.tf
 
 module "shared" {
   source = "../../shared"
 }
 
+locals {
+  frontend_path = "${path.root}/../../../frontend"
+  index_path    = "${local.frontend_path}/index.html"
+  error_path    = "${local.frontend_path}/404.html"
+  resume_path   = "${local.frontend_path}/khamkeo_khongsaly_resume.html"
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${module.shared.project_name}-stage"
-  location = module.shared.locations["stage"]
-  tags     = module.shared.tags["stage"]
+  name     = "rg-${module.shared.project_name}-${var.environment}"
+  location = module.shared.locations[var.environment]
+  tags     = module.shared.tags[var.environment]
 }
 
 module "static_site" {
   source              = "../../modules/storage_static_site"
   project_name        = module.shared.project_name
-  environment         = "stage"
+  environment         = var.environment
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  tags                = module.shared.tags["stage"]
-  index_path          = module.shared.index_path
-  error_path          = module.shared.error_path
-  resume_path         = module.shared.resume_path
+  tags                = module.shared.tags[var.environment]
+  index_path          = local.index_path
+  error_path          = local.error_path
+  resume_path         = local.resume_path
 }
