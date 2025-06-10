3d28cdb55d2c91691d23ad2924c114703b1297da
KhamkeoQuest <khamkeo@gmail.com>
2025-06-10 15:07:58 -0700

    went with the newer verison of workers instead of worker

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -2,21 +2,22 @@ data "cloudflare_zone" "main" {
   name = var.domain
 }
 
-resource "cloudflare_worker_script" "resume_forwarder" {
+resource "cloudflare_workers_script" "resume_forwarder" {
   name       = "resume-forwarder"
   account_id = var.cloudflare_account_id
   content    = file("${path.module}/worker.js")
 }
 
 
-resource "cloudflare_worker_route" "resume_route" {
+resource "cloudflare_workers_route" "resume_route" {
   zone_id     = data.cloudflare_zone.main.id
-  pattern     = "resume.${var.domain}/*"
-  script_name = cloudflare_worker_script.resume_forwarder.name
+  pattern     = "resume.khamkeokhongsaly.com/*"
+  script_name = cloudflare_workers_script.resume_forwarder.name
 }
 
-resource "cloudflare_worker_route" "www_route" {
+resource "cloudflare_workers_route" "www_route" {
   zone_id     = data.cloudflare_zone.main.id
-  pattern     = "www.${var.domain}/*"
-  script_name = cloudflare_worker_script.resume_forwarder.name
+  pattern     = "www.khamkeokhongsaly.com/*"
+  script_name = cloudflare_workers_script.resume_forwarder.name
 }
+
--- cloudflare/outputs.tf
+++ cloudflare/outputs.tf
@@ -1,3 +1,3 @@
 output "worker_script_name" {
-  value = cloudflare_worker_script.resume_forwarder.name
+  value = cloudflare_workers_script.resume_forwarder.name
 }
